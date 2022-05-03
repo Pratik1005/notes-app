f5ad4b0ef94299cf88e62a134ade8153f4fb92b0
Pratik Devle <pratik.devle98@gmail.com>
2022-05-03 16:43:31 +0530

    feat: added home route

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="home-ctn">
+      <div className="hero-ctn">
+        <h1 className="logo">MyNotes</h1>
+        <div className="hero-text">
+          <h2>Remember less, note more &amp; increase productivity</h2>
+          <div className="cta">
+            <button>Try MyNotes</button>
+          </div>
+        </div>
+      </div>
+      <div className="hero-img"></div>
+    </section>
+  );
+};
+
+export {Home};
