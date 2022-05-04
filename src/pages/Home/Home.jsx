a360ed424e7ae0b4b9e846b758912eac5a37d319
Pratik Devle <pratik.devle98@gmail.com>
2022-05-04 10:51:54 +0530

    feat: added landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,23 @@
+import "./Home.css";
+import heroImg from "../../assets/hero.svg";
+
 const Home = () => {
   return (
     <section className="home-ctn">
       <div className="hero-ctn">
         <h1 className="logo">MyNotes</h1>
-        <div className="hero-text">
-          <h2>Remember less, note more &amp; increase productivity</h2>
-          <div className="cta">
-            <button>Try MyNotes</button>
-          </div>
+        <h2>Remember less, note more &amp; increase productivity</h2>
+        <p className="hero-para">
+          Manage your daily tasks and workflow in a modern way and boost your
+          efficiency with MyNotes
+        </p>
+        <div className="cta">
+          <button className="btn btn-primary">Try MyNotes</button>
         </div>
       </div>
-      <div className="hero-img"></div>
+      <div className="hero-img">
+        <img src={heroImg} alt="hero-image" className="img-responsive" />
+      </div>
     </section>
   );
 };
