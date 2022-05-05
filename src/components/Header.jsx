c17926255914a1293f17c9a6dd30ae6c9af28a37
Pratik Devle <pratik.devle98@gmail.com>
2022-05-05 15:46:38 +0530

    feat: created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="top-header">
+      <h1 className="header-logo">MyNotes</h1>
+    </header>
+  );
+};
+
+export {Header};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {NavMenu} from "./NavMenu";
 export {Footer} from "./Footer";
+export {Header} from "./Header";
