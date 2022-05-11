7d3d4d4d355c359e7ab0668d55483c43cef6fc11
Pratik Devle <pratik.devle98@gmail.com>
2022-05-11 15:48:09 +0530

    feat: added login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export {Home} from "./Home/Home";
 export {Notes} from "./Notes/Notes";
 export {Archive} from "./Archive/Archive";
 export {Trash} from "./Trash/Trash";
+export {Login} from "./Auth/Login";
