0b4d99e13cf0ae66c6a2cd8ebb5a6e3668575a5b
Pratik Devle <pratik.devle98@gmail.com>
2022-05-11 15:18:08 +0530

    feat: added auth context

---
+++ src/context/auth-context.jsx
@@ -0,0 +1,19 @@
+import {useState, useContext, createContext} from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+  const [auth, setAuth] = useState({
+    token: token ?? "",
+    isLoggedIn: token ? true : false,
+  });
+  return (
+    <AuthContext.Provider value={{auth, setAuth}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export {useAuth, AuthProvider};
--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -17,42 +17,6 @@ const Notes = () => {
           <div className="all-notes">
             <Note />
             <Note />
-            {/* <div className="note pd-sm">
-              <span className="material-icons-outlined unpin-icon icon-hover pd-xs br-full cursor-pointer">
-                push_pin
-              </span>
-              <h4 className="pd-bottom-md">Note Title</h4>
-              <p>Note text texttexttexttexttexttexttexttext</p>
-              <div className="labels-ctn">
-                <span className="label">
-                  <label className="label-chip br-full">Test</label>
-                </span>
-                <span className="label">
-                  <label className="label-chip br-full">Core concept</label>
-                </span>
-              </div>
-              <div className="note-option-ctn">
-                <span className="note-date">10 May 2022</span>
-                <div className="note-option">
-                  <span className="priority fw-bold">High</span>
-                  <span className="material-icons-outlined icon-hover pd-xs br-full cursor-pointer">
-                    palette
-                  </span>
-                  <span className="material-icons-outlined icon-hover pd-xs br-full cursor-pointer">
-                    label
-                  </span>
-                  <span className="material-icons-outlined icon-hover pd-xs br-full cursor-pointer">
-                    archive
-                  </span>
-                  <span className="material-icons-outlined icon-hover pd-xs br-full cursor-pointer">
-                    edit
-                  </span>
-                  <span className="material-icons-outlined icon-hover pd-xs br-full cursor-pointer">
-                    delete
-                  </span>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </section>
